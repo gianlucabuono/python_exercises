b156acb96473c8dc39ab4adff1b257bcd928f055
BUONO Gianluca <gianluca.buono@soprasteria.com>
2019-04-29 14:17:08 +0200

    gbuono - find implementation based on for loop

--- element_search.py
+++ element_search.py
@@ -1,6 +1,17 @@
-def find(data, elem):
-    for _elem in data:
-        if _elem == elem:
+def find(alist, item):
+    first = 0
+    last = len(alist) - 1
+
+    while first <= last:
+        midpoint = (first + last) // 2
+        if alist[midpoint] == item:
             return True
-    return False   
+        else:
+            if item < alist[midpoint]:
+                last = midpoint - 1
+            else:
+                first = midpoint + 1
+
+    return False
+
      
--- tests/element_search_test.py
+++ tests/element_search_test.py
@@ -7,12 +7,16 @@ class TestElementSearch(unittest.TestCase):
         self.data = range(0, 10)
 
     def test_item_found(self):
-        res = element_search.find(self.data, 9)
+        res = element_search.find(self.data, 3)
         self.assertTrue(res)
 
     def test_item_not_found(self):
         res = element_search.find(self.data, 19)
         self.assertFalse(res)
+
+    def test_item_found_unsorted(self):
+        res = element_search.find([9,2,3,4,1,4,42], 9)
+        self.assertTrue(res)        
 
 if __name__ == '__main__':
     unittest.main()
